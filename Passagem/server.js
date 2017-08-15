953847d7d6569f75045e01c0e6522911b85fc727
Rafael Vicio <rafaelvicio@icloud.com>
2017-08-15 19:37:29 -0300

    Alteração base banco

--- Passagem/server.js
+++ Passagem/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
-require('./config/database')('localhost/hospedagem');
+require('./config/database')('localhost/passagem');
 //require('./config/database')('mongodb://esportsteam:esportsteam@ds123752.mlab.com:23752/esports-team');
 
 http.createServer(app).listen(3000, function(){
