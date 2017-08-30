b8deb72ac40fafaba6d2cf54d9968f767857fc4b
Rafael Vicio <rafaelvicio@icloud.com>
2017-08-30 18:52:08 -0300

    Formulario

--- Front/js/controllers/hospedagem-controller.js
+++ Front/js/controllers/hospedagem-controller.js
@@ -7,16 +7,17 @@ angular.module('alurapic').controller('HosedagemController', function($scope) {
 
     $scope.submeter = function() {
         
-                    if ($scope.formulario.$valid) {
-                        cadastroDeFotos.cadastrar($scope.foto)
-                        .then(function(dados) {
-                            $scope.mensagem = dados.mensagem;
-                            if (dados.inclusao) $scope.foto = {};
-                        })
-                        .catch(function(erro) {
-                            $scope.mensagem = erro.mensagem;
-                        });
-                    }
-                };
+        if ($scope.formulario.$valid) {
+            cadastroDeFotos.cadastrar($scope.foto)
+            .then(function(dados) {
+                $scope.mensagem = dados.mensagem;
+                if (dados.inclusao) $scope.foto = {};
+            })
+            .catch(function(erro) {
+                $scope.mensagem = erro.mensagem;
+            });
+        }
+        
+    };                 
 
 });
