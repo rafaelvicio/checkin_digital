113c1dc5a4b07a4f84a29c1cffbcbe1f9528a2cd
Rafael Vicio <rafaelvicio@icloud.com>
2017-08-29 21:39:43 -0300

    Correçao router

--- Front/js/app.js
+++ Front/js/app.js
@@ -1,1 +1,13 @@
-angular.module("checkin", ["ngRoute"]);
+angular.module("checkin", ["ngRoute"])
+    .config(function($routeProvider, $locationProvider){
+
+        $locationProvider.html5Mode(true);
+
+        $routeProvider.when('/', {
+			templateUrl: '../views/index.html',
+			controller: 'HomeController'
+        });
+
+        $routeProvider.otherwise({redirectTo: '/'});
+
+    })
